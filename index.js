1488e6412774aca42704ac5b311222279c7ab4ed
piyush terkar <terkar.piyush3@gmail.com>
2021-05-15 13:16:23 +0530

    donate and recieve routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path')
 const morgan = require('morgan')
 const methodOverride = require('method-override')
 const mysql = require('mysql')
+const favicon = require('serve-favicon')
 const app = express()
 
 app.engine('ejs', ejsMate)
@@ -14,15 +15,23 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(favicon(path.join(__dirname, 'public/images/favicon.png')))
 
 app.get('/', (req, res) => {
     res.render('pages/home');
+});
+
+app.get('/donate', (req, res) => {
+    res.render('pages/donate');
 });
 
 app.post('/', (req, res) => {
     res.send("Post route")
 });
 
+app.get('/recieve', (req, res) => {
+    res.render('pages/recieve');
+});
 app.put('/', (req, res) => {
     res.send("Put route")
 });
