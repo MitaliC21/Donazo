e1624d6100ba79a5ba9aa1057214504108a47737
Mitali <mitalichougule21@gmail.com>
2021-05-19 11:57:56 +0530

    receive

--- public/js/receive.js
+++ public/js/receive.js
@@ -1,10 +1,15 @@
 /** @format */
 
-const searchResults = axios.post("/receive");
-console.log(searchResults);
+//const axios = require('axios');
+//const form = document.querySelector('.sForm');
+const button = document.querySelector('#button');
+const searchid = document.querySelector('#select');
+const div = document.getElementById('div');
+
+
 
 function bloodgrp(bldgrp) {
-	let imgSource = document.getElementById("img").src;
+
 	if (bldgrp == "O+") {
 		imgSource = "images/Blood-Types_O-Positive.png";
 	} else if (bldgrp == "O-") {
@@ -19,7 +24,46 @@ function bloodgrp(bldgrp) {
 		imgSource = "images/Blood-Types_A-Negative.png";
 	} else if (bldgrp == "AB+") {
 		imgSource = "images/Blood-Types_AB-Positive.png";
-	} else if (bldgrp == "AB-") {
+	} else {
 		imgSource = "images/Blood-Types_AB-Negative.png";
 	}
+	return imgSource;
 }
+
+
+
+button.addEventListener('click', async function (e) {
+	e.preventDefault();
+
+	const searchTerm = searchid.value;
+	if (searchTerm != "") {
+		const config = { q: searchTerm };
+		const searchResults = await axios.post('/receive', config);
+
+		for (let result of searchResults.data) {
+			const card = document.createElement('div');
+			card.innerHTML = `<div class="card mt-4 mb-3 shadow"
+	style="max-width: 540px; max-height: 540px;">
+	<div class="row g-0 ms-100">
+		<div class="col-md-4">
+			<img src=${bloodgrp(result.bGroup)}
+				alt="..."
+				class="img-fluid">
+		</div>
+		<div class="col-md-8 mt-2">
+			<div class="card-body">
+				<h5 class="card-title">${result.fName}</h5>
+				<h5 class="card-title">${result.bGroup}</h5>
+				<h5 class="card-title">${result.age}</h5>
+				<a href="" class="btn btn-danger">Details</a>
+			</div>
+		</div>
+	</div>
+</div>`;
+			div.append(card);
+		}
+
+	} else {
+		alert("Please select blood group");
+	}
+})
