7137c4e6bdc5517e43f950d73ff516aaf8196a6b
piyush terkar <terkar.piyush3@gmail.com>
2021-05-20 20:56:09 +0530

    final commit

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # DBMS-Project
 Blood bank management system
 
-https://coolors.co/f6eabe-f7bc6f-f77919-960018-65000b-837b9e-131327
+RUN THESE COMMANDS TO INITIALISE DB FIRST:
+mysql> CREATE TABLE donars(_id int NOT NULL AUTO_INCREMENT , fName varchar(50) NOT NULL, email varch
+ar(50) NOT NULL, bGroup varchar(4) NOT NULL, age int NOT NULL, state varchar(50) NOT NULL, city var
+har(50) NOT NULL, PRIMARY KEY(_id) );
+Query OK, 0 rows affected (0.74 sec)
+
+mysql> CREATE TABLE receivers(_id int NOT NULL AUTO_INCREMENT , fName varchar(50) NOT NULL, email varchar(50) NOT NULL, bGroup varchar(4) NOT NULL, age int NOT NULL, state varchar(50) NOT NULL, city varchar(50) NOT NULL, PRIMARY KEY(_id) );
+Query OK, 0 rows affected (0.57 sec)
+
+mysql> CREATE TABLE inventory(_id int NOT NULL AUTO_INCREMENT, fName varchar(50) NOT NULL, bGroup varchar(4) NOT NULL, age int NOT NULL, PRIMARY KEY(_id));
+Query OK, 0 rows affected (0.53 sec)
+
+mysql> CREATE TRIGGER insert_after_insert
+    -> AFTER INSERT ON donars
+    -> FOR EACH ROW
+    -> INSERT INTO inventory(_id, fName, bGroup, age) VALUES(new._id, new.fName, new.bGroup, new.age);
+Query OK, 0 rows affected (1.35 sec)
+
+mysql> CREATE TRIGGER del_after_insert
+    -> AFTER INSERT ON receivers
+    -> FOR EACH ROW
+    -> DELETE FROM inventory WHERE _id = new._id;
+Query OK, 0 rows affected (0.12 sec)
--- public/js/report.js
+++ public/js/report.js
@@ -1,26 +1,41 @@
 const button = document.querySelector('#button');
 const tbody = document.querySelector('tbody');
-
+const searchid = document.querySelector('#select');
 
 button.addEventListener('click', async function (e) {
+    console.log("clicked!!")
     e.preventDefault();
     remove();
     const searchTerm = searchid.value;
     if (searchTerm != "") {
         const config = { q: searchTerm };
-        const searchResults = await axios.post('/receive', config);
+        const searchResults = await axios.post('/report', config);
+        if (searchTerm === 'i') {
+            for (let result of searchResults.data) {
+                const tr = document.createElement('tr');
+                tr.innerHTML = `<th scope="row">${result._id}</th>
+                <td>${result.fName}</td>
+                <td>-</td>
+                <td>${result.bGroup}</td>
+                <td>${result.age}</td>
+                <td>-</td>
+                <td>-</td>`;
+                tbody.append(tr);
+            }
 
-        for (let result of searchResults.data) {
-            const tr = document.createElement('tr');
-            tr.innerHTML = `<th scope="row">1</th>
+        }
+        else {
+            for (let result of searchResults.data) {
+                const tr = document.createElement('tr');
+                tr.innerHTML = `<th scope="row">${result._id}</th>
             <td>${result.fName}</td>
             <td>${result.email}</td>
             <td>${result.bGroup}</td>
             <td>${result.age}</td>
             <td>${result.state}</td>
-            <td>${result.age}</td>`;
-            section.style.height = "auto";
-            tbody.append(tr);
+            <td>${result.city}</td>`;
+                tbody.append(tr);
+            }
         }
 
     } else {
